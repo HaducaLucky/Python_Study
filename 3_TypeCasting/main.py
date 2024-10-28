fd9464c2fb70798e70efa255862e167bd31a6645
LUCKY FERNANDEZ HADUCA <147017329+HaducaLucky@users.noreply.github.com>
2024-10-28 18:40:43 +0800

    Add files via upload

---
+++ 3_TypeCasting/main.py
@@ -0,0 +1,12 @@
+
+#? Typecasting = the process of converting a variable from one data type to another
+#?               str(), int(), float(), bool()
+
+name = "Linkx"
+age = 18
+gpa = 3.2
+is_student = True
+
+name = bool(name)
+
+print(name)
